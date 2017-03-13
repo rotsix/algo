a86a92c6069c9c381b99e8f6f7f1658f32b89f76
rotsix <victor.franzi@gmail.com>
2017-03-13 23:38:57 +0100

    damn, got a brain

--- ProjectEuler/problem10.py
+++ ProjectEuler/problem10.py
@@ -3,6 +3,7 @@
 # The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 # Find the sum of all the primes below two million.
 
+# no brain
 from math import *
 
 def isprime(i):
@@ -25,3 +26,21 @@ for i in range(11, 2000000, 2):
         prime.append(i)
         res += i
 print(res)
+
+
+# brain
+def primes(n):
+    L = [True] * n
+    res = []
+    for i in range(2, n):
+        if L[i] is True:
+            res.append(i)
+            for j in range(i*i, n, i):
+                L[j] = False
+    return res
+
+res = 0
+for elt in primes(2000000):
+    res += elt
+print(res)
+
