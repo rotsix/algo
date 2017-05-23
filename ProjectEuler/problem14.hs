56acbc6f4e3b3a4dcf923eb6698cf6a794888571
rotsix <victor.franzi@gmail.com>
2017-05-24 00:05:40 +0200

    problem14

---
+++ ProjectEuler/problem14.hs
@@ -0,0 +1,23 @@
+-- The following iterative sequence is defined for the set of positive integers:
+-- n → n/2 (n is even)
+-- n → 3n + 1 (n is odd)
+-- Using the rule above and starting with 13, we generate the following sequence:
+-- 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
+-- It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+-- Which starting number, under one million, produces the longest chain?
+-- NOTE: Once the chain starts the terms are allowed to go above one million.
+
+import Data.List
+import Data.Ord
+
+syracuse :: [Integer] -> Integer -> [Integer]
+syracuse x 1 = x
+syracuse x y
+  | even y    = syracuse (y : x) (div y 2)
+  | otherwise = syracuse (y : x) (y*3 + 1)
+
+main :: IO()
+main = do
+  putStrLn $ show $ maximumBy (comparing length) res
+  where
+    res = map (syracuse []) [1..1000000]
