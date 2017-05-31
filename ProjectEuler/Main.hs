9789c7e8a4b5c2ac3fb750b1c9085c9894985030
rotsix <victor.franzi@gmail.com>
2017-05-31 16:25:31 +0200

    problem15 - solved

--- ProjectEuler/Main.hs
+++ ProjectEuler/Main.hs
@@ -19,5 +19,4 @@ usage = "Main <problem's number>"
 runSolution :: Int -> IO String
 runSolution 12 = return $ show P12.answer
 runSolution 14 = return $ show P14.answer
-runSolution 15 = return $ show P15.answer
 runSolution _ = return $ show "This problem isn't solved using Haskell"
--- ProjectEuler/problem15.py
+++ ProjectEuler/problem15.py
@@ -3,14 +3,24 @@
 # Starting in the top left corner of a 2×2 grid, and only being able to move to the right and down, there are exactly 6 routes to the bottom right corner.
 # How many such routes are there through a 20×20 grid?
 
-def dest(x, y, max):
-    print(f" x:{x} y:{y}")
-    if x == max and y == max:
-        return 1
-    if x == max:
-        return dest(x, y+1, max)
-    if y == max:
-        return dest(x+1, y, max)
-    return dest(x+1, y, max) + dest(x, y+1, max)
+# +-+-+
+# | | |
+# +-+-+
+# | | |
+# +-+-+
 
-print(dest(0, 0, 20))
+# 2×2 grid, but 9 corners (3×3)
+
+MAX = 21
+
+map = [[0] * MAX] * MAX
+
+for i in range(len(map)):
+    for j in range(len(map[i])):
+        if i == 0 or j == 0:
+            map[i][j] = 1
+        else:
+            map[i][j] = map[i-1][j] + map[i][j-1]
+
+print(map)
+print(map[MAX-1][MAX-1])
