09252e5f41920f4464f5ef0e33c8a7bbc8141719
rotsix <victor.franzi@gmail.com>
2017-05-31 15:08:35 +0200

    correct error text

--- ProjectEuler/Main.hs
+++ ProjectEuler/Main.hs
@@ -2,6 +2,7 @@ import System.Environment (getArgs)
 
 import Control.Monad (liftM)
 
+import Problem12 as P12
 import Problem14 as P14
 import Problem15 as P15
 
@@ -16,6 +17,7 @@ usage :: String
 usage = "Main <problem's number>"
 
 runSolution :: Int -> IO String
+runSolution 12 = return $ show P12.answer
 runSolution 14 = return $ show P14.answer
 runSolution 15 = return $ show P15.answer
-runSolution _ = undefined
+runSolution _ = return $ show "This problem isn't solved using Haskell"
