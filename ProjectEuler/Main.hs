09791ef23affd2da4d74b69bc051308d057f0235
rotsix <victor.franzi@gmail.com>
2017-05-31 23:18:48 +0200

    trololo Problem3

--- ProjectEuler/Main.hs
+++ ProjectEuler/Main.hs
@@ -2,9 +2,9 @@ import System.Environment (getArgs)
 
 import Control.Monad (liftM)
 
+import Problem3 as P3
 import Problem12 as P12
 import Problem14 as P14
-import Problem15 as P15
 
 main :: IO ()
 main = do
@@ -17,6 +17,7 @@ usage :: String
 usage = "Main <problem's number>"
 
 runSolution :: Int -> IO String
+runSolution 3 = return $ show P3.answer
 runSolution 12 = return $ show P12.answer
 runSolution 14 = return $ show P14.answer
 runSolution _ = return $ show "This problem isn't solved using Haskell"
