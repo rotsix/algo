06b60bd10e04492cf076b9691a8bac4745abcf6f
rotsix <victor.franzi@gmail.com>
2017-06-01 18:43:33 +0200

    Problem21 - solved

--- ProjectEuler/Main.hs
+++ ProjectEuler/Main.hs
@@ -6,20 +6,22 @@ import Problem003 as P3
 import Problem012 as P12
 import Problem014 as P14
 import Problem020 as P20
+import Problem021 as P21
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
-    [x] -> runSolution (read x) >>= putStrLn
+    [x] -> sol (read x) >>= putStrLn
     _ -> print usage
 
 usage :: String
 usage = "Main <problem's number>"
 
-runSolution :: Int -> IO String
-runSolution 3 = return $ show P3.answer
-runSolution 12 = return $ show P12.answer
-runSolution 14 = return $ show P14.answer
-runSolution 20 = return $ show P20.answer
-runSolution _ = return $ show "This problem isn't solved using Haskell"
+sol :: Int -> IO String
+sol 3 = return $ show P3.answer
+sol 12 = return $ show P12.answer
+sol 14 = return $ show P14.answer
+sol 20 = return $ show P20.answer
+sol 21 = return $ show P21.answer
+sol _ = return $ show "This problem isn't solved using Haskell"
---
+++ ProjectEuler/Problem021.hs
@@ -0,0 +1,25 @@
+module Problem021 (answer) where
+
+-- Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
+-- If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
+
+-- For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
+
+-- Evaluate the sum of all the amicable numbers under 10000.
+
+-- community/haskell-primes
+import Data.Numbers.Primes
+import Data.List
+import Data.Tuple
+
+answer :: Integer
+answer = sum [i | i <- [2..10000], isAmicable i]
+  where
+    -- amicable n = sum of the divisors of n
+    isAmicable n = let an = amicable n in (n /= an) && (amicable an == n)
+    amicable n = sum $ map product $ init $ rmdup $ subsequences $ primeFactors n
+
+rmdup :: Ord a => [a] -> [a]
+rmdup [] = []
+rmdup (x : xs) = x : (rmdup $ filter (\y -> not (x == y)) xs)
+
