8d2417da78a31e426b573dc069e1ff155b32ef45
rotsix <victor.franzi@gmail.com>
2017-06-02 17:17:57 +0200

    Problem24 - solved

--- ProjectEuler/Main.hs
+++ ProjectEuler/Main.hs
@@ -7,6 +7,8 @@ import Problem012 as P12
 import Problem014 as P14
 import Problem020 as P20
 import Problem021 as P21
+import Problem023 as P23
+import Problem024 as P24
 
 main :: IO ()
 main = do
@@ -24,4 +26,6 @@ sol 12 = return $ show P12.answer
 sol 14 = return $ show P14.answer
 sol 20 = return $ show P20.answer
 sol 21 = return $ show P21.answer
+sol 23 = return $ show P23.answer
+sol 24 = return $ show P24.answer
 sol _ = return $ show "This problem isn't solved using Haskell"
--- ProjectEuler/Utils.hs
+++ ProjectEuler/Utils.hs
@@ -4,9 +4,16 @@ module Utils (rmdup, divisors) where
 import Data.Numbers.Primes
 import Data.List
 
+-- remove duplicates : [[1,3], [1,3], [2,2]] -> [[1,3], [2,2]]
 rmdup :: Ord a => [a] -> [a]
 rmdup [] = []
 rmdup (x : xs) = x : (rmdup $ filter (\y -> not (x == y)) xs)
 
+-- list of divisors : 28 -> [2, 2, 7]
 divisors :: Integer -> [Integer]
 divisors n = map product $ init $ rmdup $ subsequences $ primeFactors n
+
+-- array to integer : [1, 2, 3] -> 123
+int :: Integer -> [Integer] -> Integer
+int _ [] = 0
+int n (x : xs) = (10^n)*x + (int (n+1) xs)
