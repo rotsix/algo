adf2c637d4bc26444ed7c9235337f92b6d22d035
rotsix <victor.franzi@gmail.com>
2017-08-08 14:50:24 +0200

    oneliner

--- ProjectEuler/Problem048.py
+++ ProjectEuler/Problem048.py
@@ -3,9 +3,15 @@
 # The series, 1¹ + 2² + 3³ + ... + 10¹⁰ = 10405071317.
 # Find the last ten digits of the series, 1¹ + 2² + 3³ + ... + 1000¹⁰⁰⁰.
 
-sum = 0
+def pretty():
+    sum = 0
+    
+    for i in range(1,1001):
+        sum += i**i
+    
+    print(str(sum)[-10:])
 
-for i in range(1,1001):
-    sum += i**i
+def oneliner():
+    print(str(sum([i**i for i in range(1,1001)]))[-10:])
 
-print(str(sum)[-10:])
+oneliner()
