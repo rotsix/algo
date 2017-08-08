1df816a613fbf35591b7e0e259fd05720cb54ce2
rotsix <victor.franzi@gmail.com>
2017-08-08 14:43:23 +0200

    Problem048 - solved

---
+++ ProjectEuler/Problem048.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+# The series, 1¹ + 2² + 3³ + ... + 10¹⁰ = 10405071317.
+# Find the last ten digits of the series, 1¹ + 2² + 3³ + ... + 1000¹⁰⁰⁰.
+
+sum = 0
+
+for i in range(1,1001):
+    sum += i**i
+
+print(str(sum)[-10:])
