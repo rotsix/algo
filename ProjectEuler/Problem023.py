38405ce492276bd32876ea74c6088d91d13a938d
Victor Franzi <victor.franzi@gmail.com>
2020-08-06 19:07:33 +0200

    problem023

---
+++ ProjectEuler/Problem023.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+# A perfect number is a number for which the sum of its proper divisors is exactly equal to the number. For example, the sum of the proper divisors of 28 would be 1 + 2 + 4 + 7 + 14 = 28, which means that 28 is a perfect number.
+
+# A number n is called deficient if the sum of its proper divisors is less than n and it is called abundant if this sum exceeds n.
+
+# As 12 is the smallest abundant number, 1 + 2 + 3 + 4 + 6 = 16, the smallest number that can be written as the sum of two abundant numbers is 24. By mathematical analysis, it can be shown that all integers greater than 28123 can be written as the sum of two abundant numbers. However, this upper limit cannot be reduced any further by analysis even though it is known that the greatest number that cannot be expressed as the sum of two abundant numbers is less than this limit.
+
+# Find the sum of all the positive integers which cannot be written as the sum of two abundant numbers.
+
+import math
+from utils import get_divisors
+
+MAX = 28123
+
+
+def isAbundantSum(x, y):
+    if x <= 0 or y >= MAX:
+        return False
+    return (abundants[x] and abundants[y]) or isAbundantSum(x - 1, y + 1)
+
+
+isAbundant = lambda n: n < sum(get_divisors(n)) - n
+abundants = {n: isAbundant(n) for n in range(1, MAX)}
+abundant_sums = {n: False for n in range(1, MAX)}
+for i in filter(lambda x: abundants[x], abundants):
+    for j in filter(lambda x: abundants[x], abundants):
+        if i + j <= MAX:
+            abundant_sums[i + j] = True
+
+print(sum(filter(lambda x: not abundant_sums[x], abundant_sums)))
---
+++ ProjectEuler/utils.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import collections
+import itertools
+
+
+def prime_factors(n):
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
+            n /= i
+            yield i
+        else:
+            i += 1
+
+    if n > 1:
+        yield n
+
+
+def prod(iterable):
+    result = 1
+    for i in iterable:
+        result *= i
+    return result
+
+
+def get_divisors(n):
+    pf = prime_factors(n)
+    pf_with_multiplicity = collections.Counter(pf)
+
+    powers = [
+        [factor ** i for i in range(count + 1)]
+        for factor, count in pf_with_multiplicity.items()
+    ]
+
+    for prime_power_combo in itertools.product(*powers):
+        yield prod(prime_power_combo)
